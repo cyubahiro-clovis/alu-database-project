71e45daba83a519f8b4ac639acd2ae5468f69f57
Vladimir-Ndayisaba <v.ndayisaba@alustudent.com>
2026-07-17 22:53:23 +0200

    Add database JOIN queries

--- alu_database.sql
+++ alu_database.sql
@@ -237,3 +237,51 @@ SELECT *
 FROM Classroom
 WHERE capacity >= 30;
 
+SELECT CONCAT(
+    s.name,
+    ' is enrolled in ',
+    c.course_name,
+    ', taught by ',
+    f.name,
+    ', in classroom ',
+    cl.room_number,
+    '.'
+) AS student_course_information
+FROM Students s
+JOIN Student_Courses sc
+    ON s.student_id = sc.student_id
+JOIN Courses c
+    ON sc.course_id = c.course_id
+JOIN Faculty f
+    ON c.faculty_id = f.faculty_id
+JOIN Classroom cl
+    ON c.classroom_id = cl.classroom_id;
+
+
+SELECT CONCAT(
+    s.name,
+    ' participates in ',
+    a.activity_name,
+    ', advised by ',
+    f.name,
+    '.'
+) AS student_activity_information
+FROM Students s
+JOIN Student_Activities sa
+    ON s.student_id = sa.student_id
+JOIN Extra_Curricular_Activities a
+    ON sa.activity_id = a.activity_id
+JOIN Faculty f
+    ON a.faculty_advisor_id = f.faculty_id;
+
+SELECT CONCAT(
+    s.name,
+    ' is assigned to classroom ',
+    cl.room_number,
+    ' in ',
+    cl.building,
+    '.'
+) AS student_classroom_information
+FROM Students s
+JOIN Classroom cl
+    ON s.classroom_id = cl.classroom_id;
